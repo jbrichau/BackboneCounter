cdd6097e766cfbff378dbf80427279258fb206af
Johan Brichau <johan@yesplan.be>
2012-12-23 15:18:18 +0100

    Got something working
    Renders the initial state

--- js/app.js
+++ js/app.js
@@ -1,24 +1,51 @@
-function(){
+var app = app || {};
+	
+(function(){
 
   var Counter = Backbone.Model.extend({
 
       defaults: {
-        value: 0
-      }
+        countervalue: 0
+      },
 
       increment: function() {
-         this.value += 1;
-      }
+         this.countervalue += 1;
+      },
 
       decrement: function() {
-         this.value -= 1;
+         this.countervalue -= 1;
       }
 
   });
 
   var CounterView = Backbone.View.extend({
 
-    
- })
+    tagName: 'h1',
 
+    render: function() {
+      this.$el.html(this.model.get('countervalue'));
+      return this;
+    }
+    
+  })
+
+  var AppView = Backbone.View.extend({
+
+    el: '#backbonecounter',
+    counterview: new CounterView({model: new Counter()}),
+
+    //initialize: function() {
+      //counterview = new CounterView({ model: Counter });
+    //},
+
+    render: function() {
+      this.$el.prepend(this.counterview.render().el);
+      return this;
+    }
+	
+  })
+
+  // Kick things off by creating the **App**.
+  $(function(){ app = new AppView().render()});
 }());
+
