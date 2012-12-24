f9a25c5faea5d9b6493e1a6b01ae1baadfd3e5f7
Johan Brichau <johan@yesplan.be>
2012-12-24 14:25:18 +0100

    Skeleton couchapp
    First steps into the couchapp world

---
+++ couchapp/backbone-counter/_attachments/js/app.js
@@ -0,0 +1,91 @@
+var app;
+
+Backbone.couch_connector.config.db_name = "backbone-counter";
+Backbone.couch_connector.config.ddoc_name = "backbone-counter";
+Backbone.couch_connector.config.global_changes = false;
+	
+(function(){
+
+  var Counter = Backbone.Model.extend({
+
+      defaults: {
+        countervalue: 0
+      },
+
+      increment: function() {
+         this.save({countervalue: this.get('countervalue') + 1});
+      },
+
+      decrement: function() {
+         this.save({countervalue: this.get('countervalue') - 1});
+      }
+
+  });
+
+  var Counters = Backbone.Collection.extend({
+     
+     model: Counter,
+
+     url : "/counters",
+
+     initialize: function() {
+        this.fetch();
+     }
+  });
+
+  var theCounters = new Counters();
+
+  var CounterView = Backbone.View.extend({
+
+    tagName: 'h1',
+
+    initialize: function() {
+      this.model.on( 'change', this.render, this );
+    },
+
+    render: function() {
+      this.$el.html(this.model.get('countervalue'));
+      return this;
+    }
+    
+  })
+
+  var AppView = Backbone.View.extend({
+
+    el: '#backbonecounter',
+
+    events:{
+	    "click .plus" : "plus",
+	    "click .min" : "min"
+    },
+
+    initialize: function() {
+	  var currentCounter;
+	  if(theCounters.size() > 0)
+		currentCounter = theCounters.last();
+	  else {
+	    currentCounter = new Counter();
+	    theCounters.push(currentCounter);
+	  }
+      this.counterview = new CounterView({ model: currentCounter });
+    },
+
+    render: function() {
+      this.$el.prepend(this.counterview.render().el);
+      return this;
+    },
+
+    plus: function() {
+	  this.counterview.model.increment();
+    },
+
+    min: function() {
+	  this.counterview.model.decrement();
+    }
+	
+  })
+
+  // Kick things off by creating the **App**.
+  $(function(){ app = new AppView().render()});
+}());
+
